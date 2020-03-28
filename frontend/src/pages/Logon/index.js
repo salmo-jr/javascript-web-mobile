6a581ef8559276bedc8563ec88e46cd6ad4eeb8c
salmo-jr <salmo.jr@gmail.com>
2020-03-28 14:10:48 -0300

    conclusão da aplicação web

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
-import './styles.css';
+import { Link, useHistory } from 'react-router-dom';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
+import './styles.css';
 
 function Logon(){
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+        try{
+            const response = await api.post('session', {id});
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+            history.push('/profile');
+        } catch(err){
+            alert('Falha no login, tente novamente');
+        }
+    }
+
     return(
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero" />
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
-                    <input type="text" placeholder="Sua ID" />
+                    <input
+                        type="text"
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)} />
                     <button className="button" type="submit">Entrar</button>
                     <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft} from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
 import './styles.css';
 
 function NewIncident(){
+    const history = useHistory();
+    const ongId = localStorage.getItem('ongId');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState(0.00);
+
+    async function handleNewIncident(e){
+        e.preventDefault();
+        const data = {
+            title,
+            description,
+            value
+        };
+
+        try{
+            await api.post('incidents', data, {
+                headers:{Authorization: ongId}
+            });
+            history.push('/profile');
+        } catch(err){
+            alert('Erro ao cadastrar novo caso, tente novamente');
+        }
+    }
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -18,10 +43,19 @@ function NewIncident(){
                     </Link>
                 </section>
 
-                <form>
-                    <input type="text" placeholder="Título do caso" />
-                    <textarea placeholder="Descrição" />
-                    <input type="number" min="0" placeholder="Valor (R$)" />
+                <form onSubmit={handleNewIncident}>
+                    <input type="text"
+                        placeholder="Título do caso"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)} />
+                    <textarea
+                        placeholder="Descrição"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)} />
+                    <input type="number" min="0.00" step="0.50"
+                        placeholder="Valor (R$)"
+                        value={value}
+                        onChange={e => setValue(e.target.value)} />
 
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
